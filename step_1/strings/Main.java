de47f2449b15acd846cded82a3fa95781a9b1381
Supriya Debnath <0xsupriya@gmail.com>
2025-07-23 20:47:15 +0530

    "Strings problem"

---
+++ step_1/strings/Main.java
@@ -0,0 +1,16 @@
+// Write a Java program to find the vowels in a given string.
+
+package step_1.strings;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Supriya Debnath";
+        for (char i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println(name.charAt(i));
+            }
+        }
+    }
+}
