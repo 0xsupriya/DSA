83044af212191322386898c30b136a5fb1cf7369
Supriya Debnath <0xsupriya@gmail.com>
2025-07-23 20:17:32 +0530

    "switch-case problem"

---
+++ step_1/switch_case/Main.java
@@ -0,0 +1,40 @@
+package step_1.switch_case;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Day no. :");
+        int day = scanner.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
+        scanner.close();
+    }
+}
