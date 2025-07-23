153b01868853c3c1fc5b6c2bb2fd992549a57e24
Supriya Debnath <0xsupriya@gmail.com>
2025-07-23 20:26:48 +0530

    "datatypes problem"

---
+++ step_1/data_types/Main.java
@@ -0,0 +1,24 @@
+package step_1.data_types;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Length: ");
+        double length = scanner.nextInt();
+
+        System.out.println("Enter Breadth: ");
+        double breadth = scanner.nextInt();
+
+        double area = length * breadth;
+        System.out.println("Area of a recangle is: " + area);
+
+        double perimeter = 2 * (length + breadth);
+        System.out.println("Perimeter of a recangle is: " + perimeter);
+
+        scanner.close();
+    }
+}
