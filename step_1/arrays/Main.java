9dc4439764142d5f78999d6fae4bceb52adf9659
Supriya Debnath <0xsupriya@gmail.com>
2025-07-23 20:36:58 +0530

    "Array Problem"

---
+++ step_1/arrays/Main.java
@@ -0,0 +1,16 @@
+// Write a Java program to find the elements in the array are even numbers.
+
+package step_1.arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
