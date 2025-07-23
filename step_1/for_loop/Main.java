1c5cc5a6c95799ce5aa0909b3d1662497e7b1e44
Supriya Debnath <0xsupriya@gmail.com>
2025-07-23 21:06:18 +0530

    "For loop problem"

---
+++ step_1/for_loop/Main.java
@@ -0,0 +1,21 @@
+//Take a integer as an input and print the multiplication table of that number using a for loop.
+
+package step_1.for_loop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter table of a number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(n * i);
+        }
+
+        scanner.close();
+    }
+}
