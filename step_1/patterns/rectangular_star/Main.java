6638c6baa048992174c11c1f55995d9ab0eb06c7
Supriya Debnath <0xsupriya@gmail.com>
2025-07-24 14:07:24 +0530

    "rectangular star problem"

---
+++ step_1/patterns/rectangular_star/Main.java
@@ -0,0 +1,20 @@
+package step_1.patterns.rectangular_star;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
