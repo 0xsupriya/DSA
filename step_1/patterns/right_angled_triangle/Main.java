e1acb91a82590dd125026d00823f568bbff8d0d6
Supriya Debnath <0xsupriya@gmail.com>
2025-07-24 15:06:28 +0530

    "right angled triangle problem"

---
+++ step_1/patterns/right_angled_triangle/Main.java
@@ -0,0 +1,19 @@
+package step_1.patterns.right_angled_triangle;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
