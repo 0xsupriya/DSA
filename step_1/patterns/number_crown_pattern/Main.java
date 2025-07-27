e22fb241de9efda0f9b2f2a511401e85e118c81f
Supriya Debnath <0xsupriya@gmail.com>
2025-07-28 04:58:08 +0530

    "Number Crown Pattern"

---
+++ step_1/patterns/number_crown_pattern/Main.java
@@ -0,0 +1,28 @@
+package step_1.patterns.number_crown_pattern;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // number
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            // space
+            for (int j = 1; j <= 2 * n - 2 * i; j++) {
+                System.out.print(" ");
+            }
+            // number
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
