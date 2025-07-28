8d5bc6713dc03105b496c894600c7c80aa1d86ce
Supriya Debnath <0xsupriya@gmail.com>
2025-07-28 06:44:40 +0530

    "Reverse Letter Triangle Pattern"

---
+++ step_1/patterns/reverse_letter_triangle_pattern/Main.java
@@ -0,0 +1,19 @@
+package step_1.patterns.reverse_letter_triangle_pattern;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (char ch = 'A'; ch <= 'A' + (n - i); ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
