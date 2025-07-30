621df6e572e5418450651c89b3b88dccd27c4591
Supriya Debnath <0xsupriya@gmail.com>
2025-07-30 14:35:59 +0530

    fixed alpha ramp pattern

--- step_1/patterns/alpha_ramp_pattern/Main.java
+++ step_1/patterns/alpha_ramp_pattern/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.print("Enter a number: ");
         int n = scanner.nextInt();
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             char ch = (char) ('A' + i);
             for (int j = 0; j <= i; j++) {
                 System.out.print(ch);
