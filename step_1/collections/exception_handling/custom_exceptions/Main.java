33fd289840adc88d22f878101506494d578bc93c
Supriya Debnath <0xsupriya@gmail.com>
2025-08-03 12:04:14 +0530

    "Custom Exception"

---
+++ step_1/collections/exception_handling/custom_exceptions/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void checkAge(int age) throws MyException {
+        if (age > 18) {
+            System.out.println("You're Adult");
+        } else {
+            throw new MyException("You're not Adult");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(16);
+        } catch (MyException e) {
+            System.out.println("caught " + e);
+        }
+    }
+}
