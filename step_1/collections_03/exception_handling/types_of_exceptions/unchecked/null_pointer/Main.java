3d63fb3875c26fa7416d584935d1bb81790754b3
Supriya Debnath <0xsupriya@gmail.com>
2025-08-03 15:19:43 +0530

    "Null Pointer Exception"

---
+++ step_1/collections_03/exception_handling/types_of_exceptions/unchecked/null_pointer/Main.java
@@ -0,0 +1,9 @@
+package step_1.collections_03.exception_handling.types_of_exceptions.unchecked.null_pointer;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = null;
+        int length = str.length();
+        System.out.println(length); // NullPointerException
+    }
+}
