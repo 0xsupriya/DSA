b4561fe5de50d710fea6421ab91cc18f6eb0783f
Supriya Debnath <0xsupriya@gmail.com>
2025-08-03 05:43:21 +0530

    "Throw Keyworld"

---
+++ step_1/collections/exception_handling/throw_keyword/Main.java
@@ -0,0 +1,8 @@
+package step_1.collections.exception_handling.throw_keyword;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        throw new ArithmeticException("Throw manually Exception");
+    }
+}
