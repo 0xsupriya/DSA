30e8af1f5fcdbc6f6bf1feb20f5653912bcdb143
Supriya Debnath <0xsupriya@gmail.com>
2025-08-06 03:33:23 +0530

    array list

---
+++ step_1/collections_03/collection_framework/list/array_list/Main.java
@@ -0,0 +1,16 @@
+package step_1.collections_03.collection_framework.list.array_list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // made a arrayList
+        List<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Supriya");
+        arrayList.add("Arijit");
+
+        System.out.print(arrayList);
+    }
+}
