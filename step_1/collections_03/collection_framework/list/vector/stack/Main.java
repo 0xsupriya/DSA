956b8c72ed71a14a8daca4df0b2fe539913bea7b
Supriya Debnath <0xsupriya@gmail.com>
2025-08-06 04:25:10 +0530

    vector: stack

---
+++ step_1/collections_03/collection_framework/list/vector/stack/Main.java
@@ -0,0 +1,18 @@
+package step_1.collections_03.collection_framework.list.vector.stack;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack);
+
+    }
+}
