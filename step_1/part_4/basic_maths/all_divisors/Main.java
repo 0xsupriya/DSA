7d818a62cf826cdacc51ce7615eb699ffe15097f
Supriya Debnath <0xsupriya@gmail.com>
2025-08-07 16:03:11 +0530

    find all Divisors

---
+++ step_1/part_4/basic_maths/all_divisors/Main.java
@@ -0,0 +1,17 @@
+package step_1.part_4.basic_maths.all_divisors;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
