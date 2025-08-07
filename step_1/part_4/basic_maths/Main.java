12ab36be5e523787135755072d815b6c8e518969
Supriya Debnath <0xsupriya@gmail.com>
2025-08-07 11:15:25 +0530

    count of Digit

--- step_1/part_4/basic_maths/Main.java
+++ step_1/part_4/basic_maths/Main.java
@@ -1,5 +1,18 @@
 package step_1.part_4.basic_maths;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int digit = scanner.nextInt();
+        int count = 0;
+        while (digit > 0) {
+            count = count + 1;
+            digit = digit / 10;
+        }
+        System.out.println("Count of Digit is: " + count);
+        scanner.close();
+    }
 }
