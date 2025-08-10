ac152caf1fc262d3af2e299e253a2a1574066b42
Supriya Debnath <0xsupriya@gmail.com>
2025-08-11 04:35:32 +0530

    printing n to 1 by recursion

---
+++ step_1/part_5/basic_recursion/print_n_to_1/Main.java
@@ -0,0 +1,21 @@
+package step_1.part_5.basic_recursion.print_n_to_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = scanner.nextInt();
+        number(1, n);
+        scanner.close();
+    }
+
+    public static void number(int i, int n) {
+        if (i > n)
+            return;
+        System.out.println(n);
+
+        number(i, n - 1);
+    }
+}
