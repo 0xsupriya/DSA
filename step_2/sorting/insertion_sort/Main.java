fc524e9feebfae7c316f4d17087799d17e06278a
Supriya Debnath <0xsupriya@gmail.com>
2025-08-17 06:58:18 +0530

    Insertion sort

---
+++ step_2/sorting/insertion_sort/Main.java
@@ -0,0 +1,21 @@
+package step_2.sorting.insertion_sort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = { 15, 46, 24, 52, 20, 9 };
+        int n = array.length;
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && array[j - 1] > array[j]) {
+                int temp = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+        System.out.println("After Selection sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
