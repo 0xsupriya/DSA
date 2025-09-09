a40074ba9143ca08b813b0e932d8448f68dbc078
Supriya Debnath <0xsupriya@gmail.com>
2025-09-09 12:27:11 +0530

    Second Largest Elemeny of Array

---
+++ step_3/arrays/easy/second_largest/Main.java
@@ -0,0 +1,24 @@
+package step_3.arrays.easy.second_largest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(second_largest());
+    }
+
+    public static int second_largest() {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int largest = arr[0];
+        int secondLargest = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}
