cfde2f428ab03785dc41b923eee0d5f001cd0033
Supriya Debnath <0xsupriya@gmail.com>
2025-09-09 12:52:11 +0530

    Second Smallest Element  in an Array

---
+++ step_3/arrays/easy/second_smallest/Main.java
@@ -0,0 +1,23 @@
+package step_3.arrays.easy.second_smallest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(second_smallest());
+    }
+
+    public static int second_smallest() {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int smallest = arr[0];
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] != smallest && arr[i] < secondSmallest) {
+                secondSmallest = arr[i];
+            }
+        }
+        return secondSmallest;
+    }
+}
